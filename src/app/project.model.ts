352d3a7308ff8e9bc404e68a0fcfde2dcd40d94e
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-05-11 09:39:35 -0700

    add project class model

---
+++ src/app/project.model.ts
@@ -0,0 +1,3 @@
+export class Project {
+   constructor (public title: string, public creator: string, public type: string, public description: string, public goal: number, public offer: string, public image: string) { }
+}
