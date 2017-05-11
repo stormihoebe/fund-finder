072831dcd051657a2dba966ecf2f79b5aefdec01
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-05-11 09:17:42 -0700

    add about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { routing } from  './app.routing';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
