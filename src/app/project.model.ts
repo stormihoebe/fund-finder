4bc86b5d5879d843a9c742bc5d0dfd67fe230d46
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-05-11 13:34:32 -0700

    add footer and add current amount raised to details, model, and json

--- src/app/project.model.ts
+++ src/app/project.model.ts
@@ -1,3 +1,3 @@
 export class Project {
-   constructor (public title: string, public creator: string, public type: string, public description: string, public snippet: string, public goal: number, public offer: string, public image: string) { }
+   constructor (public title: string, public creator: string, public type: string, public description: string, public snippet: string, public goal: number, public amount: number, public offer: string, public image: string) { }
 }
