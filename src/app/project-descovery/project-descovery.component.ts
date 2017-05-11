8e366a37dabf88041acb001aa13224f6233b0898
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-05-11 10:06:22 -0700

    add json file with initial projects

---
+++ src/app/project-descovery/project-descovery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-descovery',
+  templateUrl: './project-descovery.component.html',
+  styleUrls: ['./project-descovery.component.css']
+})
+export class ProjectDescoveryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
