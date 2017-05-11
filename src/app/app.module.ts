e0cbe96eeeb0f69e05835b74fdb5ade7fda1f019
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-05-11 09:54:46 -0700

    config app module for angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,17 @@ import { AboutComponent } from './about/about.component';
 import { ProjectDescoveryComponent } from './project-descovery/project-descovery.component';
 import { ProjectDiscoveryComponent } from './project-discovery/project-discovery.component';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
+
 
 @NgModule({
   declarations: [
@@ -23,7 +34,10 @@ import { ProjectDetailComponent } from './project-detail/project-detail.componen
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
